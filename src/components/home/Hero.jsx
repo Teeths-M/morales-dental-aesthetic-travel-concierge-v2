9236afe09759170f04040b71da47efae19a48fc1
Theon Morales <theonmorales@gmail.com>
2026-06-06 15:56:07 +0000

    File changes

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -57,7 +57,7 @@ function SafeTGlobe({ shieldState }) {
   React.useEffect(() => {
     const timer = setInterval(() => {
       setActiveCountryIndex((prev) => (prev + 1) % COUNTRIES.length);
-    }, 400);
+    }, 600);
     return () => clearInterval(timer);
   }, []);
 
