fe1f71760141519b515b81a7312de3c32ffcbe43
Theon Morales <theonmorales@gmail.com>
2026-06-10 01:27:49 +0000

    File changes

--- src/components/procedures/ProcedureSearch.jsx
+++ src/components/procedures/ProcedureSearch.jsx
@@ -163,6 +163,7 @@ export default function ProcedureSearch({ onSelect, onQueryChange }) {
         <motion.div
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
+          className="mt-4"
         >
           <SmartFallback 
             originalQuery={query}
